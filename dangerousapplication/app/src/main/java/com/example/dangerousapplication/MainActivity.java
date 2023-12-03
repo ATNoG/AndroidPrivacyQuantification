bf437b7873174e90767663552e12d39c70914c1c
mar0n0 <joaomarono@protonmail.com>
2023-12-03 14:46:15 +0000

    Dangerous app

---
+++ dangerousapplication/app/src/main/java/com/example/dangerousapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dangerousapplication;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
